044970d4215f89b847e04cf7e0890661abe25ba6
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2024-04-06 22:00:15 +0530

    Project Initialization

---
+++ Codify-Task(Our-Courses)/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      
+      <h1 className="text-center text-4xl bg-red-300">Vite + React</h1>
+      
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
